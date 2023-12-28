12aaa3443fe44f415e273e05bbd155ab27fedb43
E3Tmn <nick1tapinyagin@yandex.ru>
2023-12-28 19:22:30 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,11 +1,9 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
 
 
-def main():
-    with open('books_json', 'r', encoding='utf-8') as my_file:
-        books = json.loads(my_file.read())
-
+def on_reload(books):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
@@ -16,6 +14,14 @@ def main():
     )
     with open('index.html', 'w', encoding='utf-8') as file:
         file.write(rendered_page)
+
+
+def main():
+    with open('books_json', 'r', encoding='utf-8') as my_file:
+        books = json.loads(my_file.read())
+    on_reload(books)
+    server = Server()
+    server.serve(root='./index.html')
 
 
 if __name__ == "__main__":
