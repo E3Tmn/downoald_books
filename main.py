ab1de768e5a83bedf32afd505c2d572afaef8b50
E3Tmn <nick1tapinyagin@yandex.ru>
2023-12-30 00:33:09 +0300

    Add links to books txt files.

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def download_picture(count, book_cover_url, book_url):
     response.raise_for_status()
     image_folder = 'image'
     Path(image_folder).mkdir(exist_ok=True)
-    with open(os.path.join(image_folder, f'{count}{ext}'), 'wb') as file:
+    image_path = os.path.join(image_folder, f'{count}{ext}')
+    with open(image_path, 'wb') as file:
         file.write(response.content)
+    return image_path
 
 
 def download_book(count, book_title, book_folder, id):
@@ -36,8 +38,10 @@ def download_book(count, book_title, book_folder, id):
     check_for_redirect(book_response)
     book_name = f"{book_title}.txt"
     Path(book_folder).mkdir(exist_ok=True)
-    with open(os.path.join(book_folder, f'{count}.{book_name}'), 'wb') as file:
+    book_path = os.path.join(book_folder, f'{count}.{book_name}')
+    with open(book_path, 'wb') as file:
         file.write(book_response.content)
+    return book_path
 
 
 def parse_book_page(response):
@@ -59,8 +63,8 @@ def parse_book_page(response):
 def main():
     parser = argparse.ArgumentParser(description='''Программа позволяет скачать книги, их обложки и комментарии с сайта https://tululu.org/.
                                      Для начала работы желательно выбрать с какой страницы(start_page) по какую страницу(end_page) скачивать книги''')
-    parser.add_argument('--start_page', type=int, default=701, help='Номер первой страницы')
-    parser.add_argument('--end_page', type=int, default=702, help='Номер второй страницы')
+    parser.add_argument('--start_page', type=int, default=1, help='Номер первой страницы')
+    parser.add_argument('--end_page', type=int, default=2, help='Номер второй страницы')
     parser.add_argument('--dest_folder', default='books', help='Путь к каталогу с результатами парсинга: картинкам, книгам, JSON')
     parser.add_argument('--skip_imgs', action='store_true', help='Выбрав этот параметр Вы подтверждаете отказ от скачивания картинок')
     parser.add_argument('--skip_txt', action='store_true', help='Выбрав этот параметр Вы подтверждаете отказ от скачивания книг')
@@ -89,11 +93,13 @@ def main():
                     response.raise_for_status()
                     check_for_redirect(response)
                     book = parse_book_page(response)
-                    books.append(book)
                     if not skip_txt:
-                        download_book(count, book['book_title'], dest_folder, re.findall(r'\d+', book_link)[0])
+                        book_path = download_book(count, book['book_title'], dest_folder, re.findall(r'\d+', book_link)[0])
+                        book['book_path'] = book_path.replace("\\", "/")
                     if not skip_imgs:
-                        download_picture(count, book['book_cover_url'], link)
+                        image_path = download_picture(count, book['book_cover_url'], link)
+                        book['image_path'] = image_path.replace("\\", "/")
+                    books.append(book)
                 except requests.HTTPError:
                     print('HTTP error occurred')
                 except requests.ConnectionError:
